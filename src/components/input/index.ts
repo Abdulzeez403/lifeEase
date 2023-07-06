ca86d54c44a78d8f978931982c9a25a7bfa80c52
sodiq <abdulazeezsodiq403@gmail.com>
2023-07-06 02:42:49 +0100

    second commit

--- src/components/input/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./TextInput";
-// export * from "./SelectInput";
-// export * from "./TextEditor";
-// export * from "./Search";
-// export * from "./File";
-// export * from "./Checkbox";
-// // export * from "./datePicker";
-// export * from "./PlusMinuInput";
--- src/components/pagenation/pagination.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Pagination } from 'antd';
-
-const AppPagination: React.FC = () => <Pagination defaultCurrent={3} total={4} />;
-
-export default AppPagination;
