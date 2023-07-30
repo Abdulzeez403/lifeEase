c6254017b8243cca54cde25e5a129b6ee44974f3
sodiq <abdulazeezsodiq403@gmail.com>
2023-07-30 07:50:37 +0100

    new changes

--- src/module/booking/components/rooms.tsx
+++ src/module/booking/components/rooms.tsx
@@ -1,6 +1,6 @@
 import { ApImage } from '@/components/image';
 import React from 'react'
-import Image1 from "../../../../public/image/image1.png"
+// import Image1 from "../../../../public/image/image1.png"
 import Image2 from "../../../../public/image/image2.png"
 import Image3 from "../../../../public/image/image3.png"
 import Image4 from "../../../../public/image/image4.png"
@@ -10,7 +10,7 @@ const RoomsImage = () => {
         <div className='flex gap-x-3 py-5'>
             <div className='w-[43rem] gap-x-2'>
                 <ApImage
-                    src={Image1}
+                    src={Image4}
                     width={30}
                     height={30}
                     alt=""
